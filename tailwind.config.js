3d4949c8e17290782a48d5059bb24604b6c93cee
tranhuuloi02112003 <tranhuuloi2k3@gmail.com>
2025-08-26 10:13:18 +0700

    refactor(tailwind): migrate custom colors and fonts to @theme syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,7 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        inter: ["Inter", "sans-serif"],
-        poppins: ["Poppins", "sans-serif"],
-      },
-      colors: {
-        primary: "#db4444",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
